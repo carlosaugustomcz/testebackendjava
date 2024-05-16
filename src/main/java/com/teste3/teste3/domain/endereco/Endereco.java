cb7680b83c7f96a0a89d1494d827962206eb069c
carlosaugustomcz <carlos.augustomcz@gmail.com>
2024-05-16 14:25:28 -0300

    feat: Criacao Classe Endereco

--- src/main/java/com/teste3/teste3/domain/endereco/Endereco.java
+++ src/main/java/com/teste3/teste3/domain/endereco/Endereco.java
@@ -1,27 +1,46 @@
 package com.teste3.teste3.domain.endereco;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.teste3.teste3.domain.pessoa.Pessoa;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Entity(name = "enderecos")
-@Table(name = "enderecos")
+@Data
+@Entity
+@Table
 @Getter
 @Setter
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 
 public class Endereco {
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String logradouro;
     private String cep;
-    private Integer numero;
+    private String numero;
     private String cidade;
     private String estado;
+    private Boolean principal;
 
+    @ManyToOne
+    @JoinColumn()
+    @JsonBackReference()
+    private Pessoa pessoa;
 
+    public Endereco (String logradouro, String cep, String numero, String cidade, String estado, Boolean principal, Pessoa pessoa){
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.principal = principal;
+        this.pessoa = pessoa;
+    }
+
+    public Endereco(){
+
+    }
 }
